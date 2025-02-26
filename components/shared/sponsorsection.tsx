2af7b539a1b0d577a1bf03d62a89d04d5a0091a3
devbh04 <devbhangale.work@gmail.com>
2025-02-27 00:40:47 +0530

    initial

--- components/shared/sponsorsection.tsx
+++ components/shared/sponsorsection.tsx
@@ -4,7 +4,7 @@ import { SponsorBox } from './sponsorbox'
 const SponsorSection = () => {
   return (
     <div className="xl:-ml-10 p-6">
-        <div className="flex justify-center items-center mt-8 xl:mt-12">
+        <div className="flex justify-center items-center">
           <p className="text-3xl xl:text-5xl">Our Sponsors</p>
         </div>
 
